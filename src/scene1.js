4a97771df2ae8bcafab249dc39195e1625850927
kosinaz <kosinaz@gmail.com>
2020-01-11 21:46:56 +0100

    Re-add fullscreen

--- src/scene1.js
+++ src/scene1.js
@@ -109,7 +109,7 @@ export default class Scene1 extends Scene {
   handleEvent(event) {
     super.handleEvent(event);
     if (!document.fullscreenElement) {
-      // this.game.display.getContainer().requestFullscreen();
+      this.game.display.getContainer().requestFullscreen();
     }
     if (event.type === 'keydown') {
       clearInterval(this.interval);
